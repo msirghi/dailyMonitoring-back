4bbb5561e1ddd53176fc5928dbc6707b69ba462d
msirghi <mr.serven@yahoo.com>
2020-05-02 18:41:20 +0300

    [Add]: JWT token authorization

--- src/main/java/com/example/dailymonitoring/DailyMonitoringApplication.java
+++ src/main/java/com/example/dailymonitoring/DailyMonitoringApplication.java
@@ -1,12 +1,18 @@
 package com.example.dailymonitoring;
 
-
 import java.net.InetAddress;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
@@ -25,4 +31,34 @@ public class DailyMonitoringApplication {
     log.info("External: \t{}://{}:{}", schema, InetAddress.getLocalHost().getHostAddress(), port);
   }
 
+//  @Bean
+//  public ServletWebServerFactory servletContainer() {
+//    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//      @Override
+//      protected void postProcessContext(Context context) {
+//        SecurityConstraint securityConstraint = new SecurityConstraint();
+//        securityConstraint.setUserConstraint("CONFIDENTIAL");
+//        SecurityCollection collection = new SecurityCollection();
+//        collection.addPattern("/*");
+//        securityConstraint.addCollection(collection);
+//        context.addConstraint(securityConstraint);
+//      }
+//    };
+//    tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//    return tomcat;
+//  }
+//
+//  /*
+//  We need to redirect from HTTP to HTTPS. Without SSL, this application used
+//  port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+//  redirected to HTTPS on 8443.
+//   */
+//  private Connector httpToHttpsRedirectConnector() {
+//    Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//    connector.setScheme("http");
+//    connector.setPort(8082);
+//    connector.setSecure(false);
+//    connector.setRedirectPort(8443);
+//    return connector;
+//  }
 }
--- src/main/java/com/example/dailymonitoring/models/entities/UserEntity.java
+++ src/main/java/com/example/dailymonitoring/models/entities/UserEntity.java
@@ -20,7 +20,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 
 
-
 @Entity
 @Table(name = "USERS")
 @Data
