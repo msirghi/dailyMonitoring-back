e0d376106e0751e371832437c990416148a4bb09
Dima Socolovschi <avvadon143@gmail.com>
2020-05-02 23:08:39 +0300

    Added: Task Api

--- src/main/java/com/example/dailymonitoring/services/TaskService.java
+++ src/main/java/com/example/dailymonitoring/services/TaskService.java
@@ -2,7 +2,21 @@ package com.example.dailymonitoring.services;
 
 import com.example.dailymonitoring.models.TaskData;
 import com.example.dailymonitoring.models.entities.UserEntity;
+import java.util.List;
+import java.util.Optional;
 
 public interface TaskService {
-  TaskData createTask(TaskData taskDat , UserEntity userId);
+  TaskData createTask(TaskData taskDat, Long userId);
+
+  List<TaskData> getTask(Long userId);
+
+  TaskData getParticularTask(Long userId, Long taskId);
+
+  boolean deleteTask(Long userId, Long taskId);
+
+  List<TaskData> getDoneTasks(Long userId);
+
+  List<TaskData> getInProgressTasks(Long userId);
+
+  List<TaskData> getUndoneTasks(Long userId);
 }
--- src/main/java/com/example/dailymonitoring/services/impl/TaskServiceImpl.java
+++ src/main/java/com/example/dailymonitoring/services/impl/TaskServiceImpl.java
@@ -2,52 +2,165 @@ package com.example.dailymonitoring.services.impl;
 
 
 import com.example.dailymonitoring.models.TaskData;
-import com.example.dailymonitoring.models.UserData;
 import com.example.dailymonitoring.models.entities.TaskEntity;
 import com.example.dailymonitoring.models.entities.UserEntity;
-import com.example.dailymonitoring.models.enums.StatusType;
 import com.example.dailymonitoring.models.enums.TaskStatusType;
 import com.example.dailymonitoring.respositories.TaskRepository;
+import com.example.dailymonitoring.respositories.UserRepository;
 import com.example.dailymonitoring.services.TaskService;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import javafx.concurrent.Task;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
   private final TaskRepository taskRepository;
   private final ConversionService conversionService;
+  private final UserRepository userRepository;
 
   public TaskServiceImpl(TaskRepository taskRepository,
-      ConversionService conversionService) {
+      ConversionService conversionService, UserRepository userRepository) {
     this.taskRepository = taskRepository;
     this.conversionService = conversionService;
+    this.userRepository = userRepository;
   }
 
   @Override
-  public TaskData createTask(TaskData taskData , UserEntity userId){
-    if (userId == null) {
+  public TaskData createTask(TaskData taskData, Long userId) {
+
+    if (!userRepository.existsById(userId)) {
       return TaskData.builder().build();
     }
+    UserEntity user = userRepository.getOne(userId);
 
-    for (LocalDateTime data:taskData.getDates()
+    for (LocalDateTime data : taskData.getDates()
     ) {
       TaskEntity taskEntity = conversionService.convert(taskData, TaskEntity.class);
       assert taskEntity != null;
-      taskEntity.setUser(userId);
+      taskEntity.setUser(user);
       taskEntity.setStatus(TaskStatusType.INPROGRESS);
       taskEntity.setDate(data);
       taskData.setId(taskRepository.save(taskEntity).getId());
       taskData.setStatus(taskEntity.getStatus());
-
     }
-    return taskData; //Что ретурнить
+    return taskData;
   }
 
+  @Override
+  public List<TaskData> getTask(Long userId) {
+    List<TaskData> tasks = new ArrayList<>();
+    if (!userRepository.existsById(userId)) {
+      return tasks;
+    }
+    List<TaskEntity> data = taskRepository.getTaskByUserId(userId);
+    for (TaskEntity task : data
+    ) {
+      if (task.getStatus() == TaskStatusType.DELETED) {
+        continue;
+      }
+      TaskData taskData = conversionService.convert(task, TaskData.class);
+      List<LocalDateTime> dates = new ArrayList<>();
+      dates.add(task.getDate());
+      taskData.setDates(dates);
+      tasks.add(taskData);
+    }
+    return tasks;
+  }
+
+  @Override
+  public TaskData getParticularTask(Long userId, Long taskId) {
+    return taskRepository
+        .getTaskByUserIdAndTaskId(userId, taskId)
+        .map(task -> {
+          if (task.getId() == null || task.getUser().getId() == null
+              || task.getStatus() == TaskStatusType.DELETED) {
+            return TaskData.builder().build();
+          }
+          TaskData taskData = conversionService.convert(task, TaskData.class);
+          List<LocalDateTime> dates = new ArrayList<>();
+          dates.add(task.getDate());
+          taskData.setDates(dates);
+          return taskData;
+        })
+        .orElse(TaskData.builder().build());
+  }
+
+  @Override
+  public boolean deleteTask(Long userId, Long taskId) {
+    return taskRepository
+        .getTaskByUserIdAndTaskId(userId, taskId)
+        .map(task -> {
+          if (task.getId() == null || task.getUser().getId() == null || task.getStatus()
+              .equals(TaskStatusType.DELETED)) {
+            return false;
+          }
+          taskRepository.markAsDeleted(userId, taskId);
+          return true;
+        })
+        .orElse(false);
+  }
+
+  @Override
+  public List<TaskData> getDoneTasks(Long userId) {
+    List<TaskData> tasks = new ArrayList<>();
+    if (!userRepository.existsById(userId)) {
+      return tasks;
+    }
+    List<TaskEntity> data = taskRepository.getTaskByUserId(userId);
+    for (TaskEntity task : data
+    ) {
+      if (task.getStatus() == TaskStatusType.DONE) {
+        TaskData taskData = conversionService.convert(task, TaskData.class);
+        List<LocalDateTime> dates = new ArrayList<>();
+        dates.add(task.getDate());
+        taskData.setDates(dates);
+        tasks.add(taskData);
+      }
+    }
+    return tasks;
+  }
+
+  @Override
+  public List<TaskData> getInProgressTasks(Long userId) {
+    List<TaskData> tasks = new ArrayList<>();
+    if (!userRepository.existsById(userId)) {
+      return tasks;
+    }
+    List<TaskEntity> data = taskRepository.getTaskByUserId(userId);
+    for (TaskEntity task : data
+    ) {
+      if (task.getStatus() == TaskStatusType.INPROGRESS) {
+        TaskData taskData = conversionService.convert(task, TaskData.class);
+        List<LocalDateTime> dates = new ArrayList<>();
+        dates.add(task.getDate());
+        taskData.setDates(dates);
+        tasks.add(taskData);
+      }
+    }
+    return tasks;
+  }
+
+  @Override
+  public List<TaskData> getUndoneTasks(Long userId) {
+    List<TaskData> tasks = new ArrayList<>();
+    if (!userRepository.existsById(userId)) {
+      return tasks;
+    }
+    List<TaskEntity> data = taskRepository.getTaskByUserId(userId);
+    for (TaskEntity task : data
+    ) {
+      if (task.getStatus() == TaskStatusType.UNDONE) {
+        TaskData taskData = conversionService.convert(task, TaskData.class);
+        List<LocalDateTime> dates = new ArrayList<>();
+        dates.add(task.getDate());
+        taskData.setDates(dates);
+        tasks.add(taskData);
+      }
+    }
+    return tasks;
+  }
 }
+
